8b8a4b13a8338be3f31e521b9214d7be2a3cd7f1
Irina <bobrovairina1812@gmail.com>
2021-01-22 14:57:42 +0500

    answers about cinema

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-const user = prompt('What is your name?','');
-alert (`Hello, ${user}`);
+// const user = prompt('What is your name?','');
+// alert (`Hello, ${user}`);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
