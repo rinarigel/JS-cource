feebb0d0b53c09cc872d0a41d7f24d45840447f7
Irina <bobrovairina1812@gmail.com>
2021-01-24 20:19:46 +0300

    add conditions

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
 // const user = prompt('What is your name?','');
 // alert (`Hello, ${user}`);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if (4 == 9) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Too Much');
+// } else {
+//     console.log('Ok');
+// }
+const num = 49;
+
+switch (num) {
+    case 49:
+        console.log('No');
+        break;
+    case 100:
+        console.log('No');
+        break;
+    case 50:
+        console.log('True');
+        break;
+    default:
+        console.log('Not today');
+        break;
+}
