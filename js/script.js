f7042aa7c9011d14aefc534c0761e4d22cedf5a3
Irina <bobrovairina1812@gmail.com>
2021-01-25 12:01:14 +0300

    сallback function

--- js/script.js
+++ js/script.js
@@ -1,66 +1,29 @@
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
-    }
-}
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов', ''),
-              b = prompt('На сколько оцените его?', '');
-    
-        if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
+function first () {
+    setTimeout(function() {
+        console.log(1);
+    }, 1000);                   //добавили задержку в 1000мс
 }
 
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count <10) {
-        console.log('Просмотрено достаточно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 50) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count > 50) {
-        console.log('Вы киноман-любитель');
-    } else {
-        console.log('Произошла ошибка');
-    }
+function secoud() {
+    console.log(2);
 }
 
-detectPersonalLevel();
+first();
+secoud();
 
-function showDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-showDB(personalMovieDB.privat);
+//Callbak function
 
-function writeYourGenres() {
-    for (let i = 1; i <=3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genres[i - 1] = genre;
-    }
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
 }
 
-writeYourGenres();
+// learnJS('JavaScript', function() {
+//     console.log('Я прошел этот урок!');         //анонимная функция
+// });
 
-console.log(personalMovieDB);
+function done() {
+    console.log('Я прошел этот урок!'); 
+}
+
+learnJS('JavaScript', done);
