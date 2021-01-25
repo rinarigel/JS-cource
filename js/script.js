57b5b3abf049e0f2e3d7bc54824529cce5b44b52
Irina <bobrovairina1812@gmail.com>
2021-01-25 11:45:29 +0300

    function and method

--- js/script.js
+++ js/script.js
@@ -1,178 +1,66 @@
-// const user = prompt('What is your name?','');
-// alert (`Hello, ${user}`);
+let numberOfFilms;
 
-
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-// const a = prompt('Один из последних просмотренных фильмов', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов', ''),
-//       d = prompt('На сколько оцените его?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
-
-// if (4 == 9) {
-//     console.log('Ok');
-// } else {
-//     console.log('Error');
-// }
-
-// const num = 50;
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Too Much');
-// } else {
-//     console.log('Ok');
-// }
-// const num = 49;
-
-// switch (num) {
-//     case 49:
-//         console.log('No');
-//         break;
-//     case 100:
-//         console.log('No');
-//         break;
-//     case 50:
-//         console.log('True');
-//         break;
-//     default:
-//         console.log('Not today');
-//         break;
-// }
-
-// let num = 50;
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <55);
-
-// let num = 50;
-
-// for (let i = 1; i < 8; i++) {
-//     console.log(num);
-//     num++;
-// }
-
-// let num = 50;
-
-// for (let i = 1; i < 8; i++) {
-//     if (i == 6) {
-//         // break;
-//         continue;
-//     }
-//     console.log(i);
-// }
-
-// Автоматизируем процесс задачи вопросов пользователю
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
-
-// Вариант с алертами пользователю
-// if (numberOfFilms <10) {
-//     alert ('Просмотрено достаточно мало фильмов');
-// } else if (numberOfFilms > 10 && numberOfFilms < 50) {
-//     alert ('Вы классический зритель');
-// } else if (numberOfFilms > 50) {
-//     alert ('Вы киноман-любитель');
-// } else {
-//     alert ('Произошла ошибка');
-// }
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов', ''),
-//           b = prompt('На сколько оцените его?', '');
-
-//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-
-// }
-
-// // Вариант с выводами в консоль
-// if (personalMovieDB.count <10) {
-//     console.log('Просмотрено достаточно мало фильмов');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 50) {
-//     console.log('Вы классический зритель');
-// } else if (personalMovieDB.count > 50) {
-//     console.log('Вы киноман-любитель');
-// } else {
-//     console.log('Произошла ошибка');
-// }
-
-// console.log(personalMovieDB);
-
-
-
-
-//Функции
-//Функции лучше называть глаголом
-let num = 20;                       //глобальная переменная
-
-function showFirstMessage(text) {   //аргументов у функции может быть много
-    console.log(text);
-//если объявляем переменные внутри функции, то снаружи они не доступны
-//любая функция может использовать глобальные переменные внутри себя
-    num = 10;                       
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+    }
 }
+start();
 
-showFirstMessage('Hello');
-console.log(num);
-
-function calc(a, b) {
-    return (a + b);
-}
-
-console.log(calc(4,3));
-console.log(calc(4,5));
-console.log(calc(4,7));
-
-function ret() {
-    let num = 50;
-    return num;
-}
-const anothorNum = ret();
-console.log(anothorNum);
-
-
-// Function expression
-const logger = function() {
-    console.log('Hello');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-logger();                   // использовать переменную мы можем только после ее объявления
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
-// Стрелочная функция
-// у нее есть свои особенности и использовать везде ее нельзя
-const calk = (a, b) => { return a + b};
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count <10) {
+        console.log('Просмотрено достаточно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 50) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 50) {
+        console.log('Вы киноман-любитель');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+function showDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <=3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+
+console.log(personalMovieDB);
