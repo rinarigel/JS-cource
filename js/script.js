2f712665a3cf79f74b2945bda9d9c895eb8dce07
Irina <bobrovairina1812@gmail.com>
2021-01-28 15:46:02 +0500

    random cafe

--- js/script.js
+++ js/script.js
@@ -1,100 +1,33 @@
-//Следующий пример работает только с примитивными типами данных
-// let a = 5;
-//     b = a;
+// let str = 'some';
+// let strObj = new String(str);
 
-// b = b + 5;
+// console.log(typeof (str)); //string
+// console.log(typeof (strObj)); //object
 
-// console.log(b);
-// console.log(a);
+// console.dir ([1,2,3]);
 
-// const obj = {
-//     a: 5,
-//     b:1
+// const soldier = {
+//     health:400,
+//     armor:100
 // };
 
-//Передача значения по ссылке (сама структура не передается)
-// const copy = obj;
-
-//Модифицируя ссылку мы модифицируем изначальный объект
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-// function copy(mainObj) {
-//     let objCopy = {};
-
-//     let key;
-//     for (key in mainObj) {
-//         objCopy[key] = mainObj[key];
-//     }
-
-//     return objCopy;
-// }
-
-// const numbers = {
-//     a:2,
-//     b:5,
-//     c: {
-//         x:7,
-//         y:4
-//     }
+// const john = {
+//     health:100,
 // };
 
-// const newNumbers = copy(numbers);
+// john.__proto__ = soldier;
 
-// newNumbers.a = 10;
-// newNumbers.c.x = 10;
+// console.log(john.armor);
+
+let randomArray = [
+    'смоки','рэд', 'партизан','столовка','зира', 
+    'лапшичная','хеппи','пиросмани', 'шеймус',
+    'рынок', 'пельменная','покеман', 'суфра',
+    'крапива', ' хуанд','фастфуд','китайцы'
+ ];
+ 
+let rand = randomArray[Math.floor(Math.random() * randomArray.length)];
+
+console.log(rand);
 
 
-// console.log(newNumbers);
-// console.log(numbers);
-
-//Соединение нескольких объектов
-
-// const add = {
-//     d: 17,
-//     e:20
-// };
-
-// const clone = Object.assign({}, add);
-
-// clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-// const oldArray = ['a', 'b', 'c'];
-// const newArray = oldArray.slice();          //slice-метод позволяет скопировать старый массив (а не передавать ссылкой)
-
-// newArray[1] = 'khjkjhk';
-// console.log(newArray);
-// console.log(oldArray);
-
-//Разворотная функция
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejornal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-console.log(...num);
-
-const array = ['a','b'];
-const newAarray = [...array];
-
-const q = {
-    one:1,
-    two:2
-};
-
-const newObj = {...q};
-console.log(newObj);
