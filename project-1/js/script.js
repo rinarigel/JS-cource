bd2b17474c38286efed69539196064a28b569774
Irina <i.bobrova@xsolla.com>
2021-03-26 14:57:55 +0500

    actions with page

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -1,10 +1,15 @@
 /* Задания на урок:
+
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
 2) Изменить жанр фильма, поменять "комедия" на "драма"
-3) Изменить задний фон с постером фильма на изображение "bg.jpg". Оно лежит в папке img.
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 Реализовать только при помощи JS
+
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 Отсортировать их по алфавиту 
+
 5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
@@ -19,6 +24,7 @@ const movieDB = {
     ]
 };
 
+//удаляем блоки с рекламой
 const adv = document.querySelectorAll('.promo__adv img'),
       poster = document.querySelector('.promo__bg'),
       genre = poster.querySelector('.promo__genre'),
@@ -28,18 +34,23 @@ adv.forEach(item => {
     item.remove();
 });
 
+//Меняем жанр
 genre.textContent = 'драма';
 
+//Меняем фон
 poster.style.backgroundImage = 'url("img/bg.jpg")';
 
+//Скрываем список фильмов со страницы
 movieList.innerHTML = "";
 
+//Сортируем списки по алфавиту
 movieDB.movies.sort();
 
 movieDB.movies.forEach((film, i) => {
     movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
+        <li class="promo__interactive-item">${i +1} ${film}
             <div class="delete"></div>
         </li>
     `;
 });
+
